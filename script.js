293cc96f059cdb2cde37805b042672a7704663ac
Man-Chun Yu <brian0821yu@gmail.com>
2024-06-27 16:37:10 +0100

    Add get computer/human choice function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hi Mom!')
+const CHOICE = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice () {
+    let randInt = Math.floor(Math.random() * 3);
+    return CHOICE[randInt];
+}
+
+function getHumanChoice() {
+    while (true) {
+        let humanInput = prompt('Choice?');
+        let humanChoice = humanInput.toLowerCase().trim();
+        for (let i = 0; i < CHOICE.length; i++) {
+            if (humanChoice === CHOICE[i]) {
+                return humanChoice;
+            } 
+        } 
+    }
+}
+
+console.log(getHumanChoice());
