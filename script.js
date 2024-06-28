702c2663acf4f8a430c6f0e494ad8d014da3ff3c
Man-Chun Yu <brian0821yu@gmail.com>
2024-06-28 18:12:26 +0100

    Add comments and remove hardcoding.
    Use variable CHOICE.length instead of hardcoding 3

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ const ANTICHOICE = ['paper', 'scissors', 'rock'];
 
 
 function getComputerChoice () {
-    let randInt = Math.floor(Math.random() * 3);
+    let randInt = Math.floor(Math.random() * CHOICE.length);
     return CHOICE[randInt];
 }
 
 
 function getHumanChoice() {
 
+    // Loop if choice is invalid
     while (true) {
         
-        let humanInput = prompt('Choice?');
+        let humanInput = prompt('Rock/ Paper/ Scissors?');
         let humanChoice = humanInput.toLowerCase().trim();
 
+        // Check invalid input
         for (let i = 0; i < CHOICE.length; i++) {
             if (humanChoice === CHOICE[i]) {
                 return humanChoice;
@@ -28,30 +30,35 @@ function getHumanChoice() {
 
 
 function playGame() {
+
+    // No. of rounds
     const ROUNDS = 5;
 
+    // Initialize scores
     let humanScore = 0,
         computerScore = 0;
 
 
     function playRound(humanChoice, computerChoice) {
 
+        // Check draw
         if (humanChoice === computerChoice) {
             console.log('Draw!');
             return;
         }
-    
+        // Check lose
         for (let i = 0; i < CHOICE.length; i++) {
             if (humanChoice === CHOICE[i] && computerChoice === ANTICHOICE[i]) {
                 console.log(`You lose, ${computerChoice} beats ${humanChoice}!`);
                 return computerScore++;
             }
         }
-
+        // Else win
         console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
         return humanScore++;
     }
 
+    // Play 5 rounds
     for (let i = 0; i < ROUNDS; i++){
         const humanSelection = getHumanChoice();
         const computerSelection = getComputerChoice();
